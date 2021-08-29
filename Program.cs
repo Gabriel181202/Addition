a4e58292ebabec5a896f90f0cb5a540e8474afb7
gabo0 <gabo0@GABO>
2021-08-29 16:04:06 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition");
+            int a = 5;
+            int b = 8;
+            int c = a + b;
+            Console.WriteLine(a + "+" + b + "=" + c);
+
+            Console.ReadKey();
+        }
+    }
+}
